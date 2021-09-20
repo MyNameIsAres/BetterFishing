7738a0568f1fe6a2c6c3efe80f1c56e40cc50ff7
MyNameIsAres <chrisvonk@hotmail.nl>
2021-09-20 14:36:51 +0200

    :sparkles: Enabled new configuration features for CustomCreature and CustomItem class

--- src/main/java/org/geminicraft/betterfishing/loot/impl/CustomItem.java
+++ src/main/java/org/geminicraft/betterfishing/loot/impl/CustomItem.java
@@ -7,8 +7,7 @@ import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.geminicraft.betterfishing.loot.Lootable;
-
-import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 public class CustomItem implements Lootable {
@@ -17,26 +16,16 @@ public class CustomItem implements Lootable {
     private double spawnChance;
     private String itemID;
     private String name;
-    private String[] lore;
+    private List<String> lore;
     private Set<Enchantment> enchantmentSet; // TODO Test if this should be a Set or Map
-    private double dropRate;
     // Optional: Hide attributes/enchantments information, glowing
 
 
-    public CustomItem(String name, double spawnChance, String material) {
+    public CustomItem(String name, double spawnChance, String material, List<String> lore) {
         this.name = name;
         this.spawnChance = spawnChance;
         this.material = material;
-    }
-
-    public CustomItem(String material, String itemID, String name, String[] lore, Set<Enchantment> enchantmentSet, double dropRate) {
-        this.material = material;
-        this.itemID = itemID;
-        this.name = name;
         this.lore = lore;
-        this.enchantmentSet = enchantmentSet;
-        this.dropRate = dropRate;
-
     }
 
     private void createCustomItem(Entity caughtEntity) {
@@ -46,7 +35,7 @@ public class CustomItem implements Lootable {
         itemMeta.setDisplayName(name);
         caughtItem.setCustomName(name);
         caughtItem.setCustomNameVisible(true);
-//        itemMeta.setLore(Arrays.asList(lore));
+        itemMeta.setLore(lore);
         caughtItem.setItemStack(itemStack);
     }
 
@@ -54,6 +43,10 @@ public class CustomItem implements Lootable {
 
     public String getName() {
         return name;
+    }
+
+    public List<String> getLore() {
+        return lore;
     }
 
     @Override
